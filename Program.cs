a445d5000f7d336138eff76dbe8b753013d15520
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-14 23:44:06 +0530

    PlayerPosition

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 
-namespace Snake_And_Ladder
+namespace snake_Ladder
 {
-   internal class Snake_And_Ladder
+    class Program
     {
         static void Main(string[] args)
         {
-            //UC_2
-            //int position = 0;
+            //Inintializing constant
+            const int LADDER = 1;
+            const int NOPLAY = 2;
+            const int SNAKE = 3;
+
+            //UC_1
+            int position = 0;
             Random random = new Random();
+
+            //UC_2
             int noOnDie = random.Next(1, 7);
             Console.WriteLine($"No on die:{noOnDie}");
+
+            int options = random.Next(1, 4);
+
+            switch (options)
+            {
+                case LADDER:
+                    position += noOnDie;
+                    break;
+
+                case SNAKE:
+                    position -= noOnDie;
+                    break;
+
+                case NOPLAY:
+                    Console.WriteLine("POSITION" + position);
+                    break;
+            }
+            Console.WriteLine("POSITION: " + position);
         }
     }
-}
 
+}
