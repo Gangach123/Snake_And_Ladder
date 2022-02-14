05dec42767a02dc59a62c90af60bc30b8cffa291
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-15 00:02:35 +0530

    Target100

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace snake_Ladder
+namespace snake_And_Ladder
 {
-    class Program
+    class snake_And_Ladder
     {
         static void Main(string[] args)
         {
@@ -10,32 +10,36 @@ namespace snake_Ladder
             const int LADDER = 1;
             const int NOPLAY = 2;
             const int SNAKE = 3;
+            const int targetposition = 100;
 
             //UC_1
             int position = 0;
             Random random = new Random();
 
-            //UC_2
-            int noOnDie = random.Next(1, 7);
-            Console.WriteLine($"No on die:{noOnDie}");
-
-            int options = random.Next(1, 4);
-
-            switch (options)
+            while (position < 100)
             {
-                case LADDER:
-                    position += noOnDie;
-                    break;
+                int noOnDie = random.Next(1, 7);
+                Console.WriteLine($"No on die :{noOnDie}");
 
-                case SNAKE:
-                    position -= noOnDie;
-                    break;
+                int options = random.Next(1, 4);
+                switch (options)
+                {
+                    case LADDER:
+                        position += noOnDie;
+                        break;
 
-                case NOPLAY:
-                    Console.WriteLine("POSITION" + position);
-                    break;
+                    case SNAKE:
+                        position -= noOnDie;
+                        int positionofPlayer = position < 0 ? position += noOnDie : position;
+                        break;
+
+                    case NOPLAY:
+                        Console.WriteLine("POSITION: " + position);
+                        break;
+                }
+
             }
-            Console.WriteLine("POSITION: " + position);
+           Console.WriteLine("POSITION: " + position);
         }
     }
 
