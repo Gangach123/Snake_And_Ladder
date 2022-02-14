9d4d5e6ec40b446070d1a38ae632ec0230eb2c36
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-02-14 23:13:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+     class Snake_And_Ladder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake ladder game");
+           
+        }
+
+        public static void GamePlay()
+        {
+            
+            int position = 0;
+            
+        }
+    }
+}
